8f82eaf0e937fe0d4a256291db2660cfc890a973
Abdullah <AbdullahAbubakar461@gmail.com>
2023-01-02 23:16:15 +0100

    added more functionalities

--- App.js
+++ App.js
@@ -7,33 +7,63 @@ var back2Btn = document.querySelector('.step-2')
 var back3Btn = document.querySelector('.step-3')
 var back4Btn = document.querySelector('.step-4')
 
+// Aside
+
+var no1 = document.querySelector('.step .no1')
+var no2 = document.querySelector('.step .no2')
+var no3 = document.querySelector('.step .no3')
+var no4 = document.querySelector('.step .no4')
+
 
 function next1(){
-    next1Btn.style.left = '-850px';
-    next2Btn.style.left = '-450px';
-    // next.style.visibility = 'none';
+    next2Btn.style.left = '-550px';
+    next1Btn.style.visibility = 'hidden';
+    no1.style.backgroundColor = "transparent";
+    no1.style.border ='1px #fff solid' ;  
+    no2.style.backgroundColor ='var(--numbg)';
+    no2.style.border ='none';  
 }
 function next2(){
-    next3Btn.style.left = '-850px';
-    next2Btn.style.left = '-1300px';
+    next3Btn.style.left = '-1100px';
+    next2Btn.style.visibility = 'hidden';
+    no3.style.backgroundColor ='var(--numbg)';
+    no3.style.border ='none';
+    no2.style.backgroundColor = "transparent";
+    no2.style.border ='1px #fff solid' ;
 }
 function next3(){
-    next3Btn.style.left = '-1750px';
-    confirmBtn.style.left = '-1350px';
+    next3Btn.style.visibility = 'hidden';
+    confirmBtn.style.left = '-1640px';
+    no4.style.backgroundColor ='var(--numbg)';
+    no4.style.border ='none';
+    no3.style.backgroundColor = "transparent";
+    no3.style.border ='1px #fff solid' ;
 }
 // function next4(){
 //     confirmBtn.style.left = '-450px';
 // }
 // back buttons
 function back2(){
-    next1Btn.style.left = '0px';
-    back2Btn.style.left = '400px';
+    next1Btn.style.visibility = 'visible';
+    back2Btn.style.left = '0px';
+    no2.style.backgroundColor = "transparent";
+    no2.style.border ='1px #fff solid' ;  
+    no1.style.backgroundColor ='var(--numbg)';
+    no1.style.border ='none';
 }
 function back3(){
     back3Btn.style.left = '0px';
-    next2Btn.style.left = '-450px';
+    next2Btn.style.visibility = 'visible';
+    no3.style.backgroundColor = "transparent";
+    no3.style.border ='1px #fff solid' ;
+    no2.style.backgroundColor ='var(--numbg)';
+    no2.style.border ='none';
 }
 function back4(){
-    next3Btn.style.left = '-850px';
+    next3Btn.style.visibility = 'visible';
     back4Btn.style.left = '0px';
+    no4.style.backgroundColor = "transparent";
+    no4.style.border ='1px #fff solid' ;
+    no3.style.backgroundColor ='var(--numbg)';
+    no3.style.border ='none';
 }
